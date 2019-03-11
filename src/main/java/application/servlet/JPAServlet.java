24c4abb54026f2632c66916974c5ccece9d115da
Nathan Good <NathanG@magenic.com>
2019-03-11 10:14:23 -0500

    Minor cleanup to remove commented out lines and imports

--- src/main/java/application/servlet/JPAServlet.java
+++ src/main/java/application/servlet/JPAServlet.java
@@ -1,22 +1,14 @@
 package application.servlet;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-import javax.naming.NamingException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.io.PrintWriter;
