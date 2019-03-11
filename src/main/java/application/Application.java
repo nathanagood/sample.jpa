1a3681ea0e346765683ac6ea01a61249cebf3a46
Nathan Good <NathanG@magenic.com>
2019-03-11 11:26:21 -0500

    Adding basic exmaple of a Docker image that can run our executable JAR

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,6 +1,5 @@
 package application;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
